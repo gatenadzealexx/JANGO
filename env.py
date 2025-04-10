d95abecf82ad06432172be3fc5bdfcf65d9617c0
gatenadzealexx <gatenadzealexx@gmail.com>
2025-04-10 13:31:53 +0300

    Add files via upload

---
+++ env.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ebilet_db',
+        'USER': 'postgres',
+        'PASSWORD': '231998',
+        'HOST': 'localhost',
+        'PORT': '5432',
+         }
+}
+password = '231998'
